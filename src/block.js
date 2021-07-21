8243628e025424bdbc9196134792b7664b5fe273
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2021-07-20 22:50:23 -0300

    criando função de hash

--- src/block.js
+++ src/block.js
@@ -1,4 +1,4 @@
-
+const SHA256 = require('crypto-js/sha256');
 
 class Block {
     constructor(timestamp, lastHash, hash, data){
@@ -25,10 +25,14 @@ class Block {
     static mineBlock(lastBlock, data){
         const timestamp = Date.now();
         const lastHash = lastBlock.hash;
-        const hash = '';
+        const hash = this.hash(timestamp, lastHash, data);
 
         return new this(timestamp, lastHash, hash, data);
 
+    }
+
+    static hash(timestamp, lastHash, data){
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
     }
 }
 
