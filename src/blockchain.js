1ff47c03580b3f929dc07d3d47d71e9ee91edde0
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2021-07-21 22:39:43 -0300

    criando método para validar corrente

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,17 +1,35 @@
 const Block = require('./block');
 
-class Blockchain{
-    constructor(){
+class Blockchain {
+    constructor() {
         this.block = Block;
         this.chain = [this.block.genesis()];
     }
-
-    addBlock(data){
-        const lastBlock = this.chain[this.chain.length -1];
+    // método para adicionar blocos
+    addBlock(data) {
+        const lastBlock = this.chain[this.chain.length - 1];
         const newBlock = this.block.mineBlock(lastBlock, data);
 
         this.chain.push(newBlock);
         return newBlock;
+    }
+
+    // método para validar qual corrente vai ser utilizada na blockchain
+    isValidChain(chain) {
+
+        if (JSON.stringify(chain[0] !== JSON.stringify(Block.genesis()))) return false;
+
+        for (let i = 1; i < chain.length; i++) {
+
+            const block = chain[i];
+            const lastBlock = chain[i - 1];
+
+            if (block.lastHash !== lastBlock.hash || block.hash !== Block.blockHash(block))
+                return false
+                
+        }
+
+        return true;
     }
 }
 
