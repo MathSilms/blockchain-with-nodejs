d27fcfa6dd6f4b65c66fa8d8958f1b301cc39c0e
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2021-07-21 22:39:29 -0300

    criando método para validar hash

--- src/block.js
+++ src/block.js
@@ -1,7 +1,7 @@
 const SHA256 = require('crypto-js/sha256');
 
 class Block {
-    constructor(timestamp, lastHash, hash, data){
+    constructor(timestamp, lastHash, hash, data) {
         this.timestamp = timestamp;
         this.lastHash = lastHash;
         this.hash = hash;
@@ -9,21 +9,21 @@ class Block {
     }
 
 
-    toString(){
-        return`block = 
+    toString() {
+        return `block = 
                 timestamp = ${this.timestamp}
-                lastHash = ${this.lastHash.substring(0,10)}
-                hash = ${this.hash.substring(0,10)}
+                lastHash = ${this.lastHash.substring(0, 10)}
+                hash = ${this.hash.substring(0, 10)}
                 data = ${this.data}`;
     }
 
     // função para criar o bloco genesis
-    static genesis(){
-        return new this('Genesis time', '_-_-_-_-_', 'JDFINM495FHDF49384F',[]);
+    static genesis() {
+        return new this('Genesis time', '_-_-_-_-_', 'JDFINM495FHDF49384F', []);
     }
 
     // cria um novo bloco
-    static mineBlock(lastBlock, data){
+    static mineBlock(lastBlock, data) {
         const timestamp = Date.now();
         const lastHash = lastBlock.hash;
         const hash = this.hash(timestamp, lastHash, data);
@@ -33,8 +33,15 @@ class Block {
     }
 
     // função para criar o hash
-    static hash(timestamp, lastHash, data){
+    static hash(timestamp, lastHash, data) {
         return SHA256(`${timestamp}${lastHash}${data}`).toString();
+    }
+    // método bem similar á função hash, criado apra gerar validar um hash corrompido
+    static blockHash(block) {
+
+        const { timestamp, lastHash, data } = block;
+
+        return Block.hash(timestamp, lastHash, data);
     }
 }
 
