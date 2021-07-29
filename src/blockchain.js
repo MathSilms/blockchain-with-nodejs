48007164131c5ce9885267295caa927ce38e6864
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2021-07-29 12:14:51 -0300

    criando método raplaceChain

--- src/blockchain.js
+++ src/blockchain.js
@@ -5,7 +5,7 @@ class Blockchain {
         this.block = Block;
         this.chain = [this.block.genesis()];
     }
-    // método para adicionar blocos
+    // método para adicionar blocos na corrente
     addBlock(data) {
         const lastBlock = this.chain[this.chain.length - 1];
         const newBlock = this.block.mineBlock(lastBlock, data);
@@ -13,11 +13,12 @@ class Blockchain {
         this.chain.push(newBlock);
         return newBlock;
     }
-
     // método para validar qual corrente vai ser utilizada na blockchain
     isValidChain(chain) {
 
-        if (JSON.stringify(chain[0] !== JSON.stringify(Block.genesis()))) return false;
+        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
+            return false;
+
 
         for (let i = 1; i < chain.length; i++) {
 
@@ -25,11 +26,21 @@ class Blockchain {
             const lastBlock = chain[i - 1];
 
             if (block.lastHash !== lastBlock.hash || block.hash !== Block.blockHash(block))
-                return false
-                
+                return false;
+
         }
 
         return true;
+    }
+
+    // função para trabalhar o fork da corrente e decidir qual corrente vira a principal. 
+    replaceChain(newChain) {
+        if (newChain.length <= this.chain.length)
+            return console.log('Receive chain is not longer than the current chain');
+        else if(!this.isValidChain(newChain))
+            return console.log('The received chain is not valid');
+
+        this.chain = newChain;
     }
 }
 
