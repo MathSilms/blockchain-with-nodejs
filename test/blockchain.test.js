5577bebd7bf2cef707155b8cedaa8f1a43adb70b
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2021-07-29 12:15:07 -0300

    subindo cobertura de testes

--- test/blockchain.test.js
+++ test/blockchain.test.js
@@ -1,22 +1,84 @@
 const Blockchain = require('../src/blockchain');
 const Block = require('../src/block');
 
-describe('blockchain', ()=>{
+describe('blockchain', () => {
     let blockchain;
+    let secondBlockchain;
 
-    beforeEach(()=>{
+    beforeEach(() => {
         blockchain = new Blockchain;
+        // segunda instância da blockchain
+        secondBlockchain = new Blockchain;
     })
-    
-    it('the first block is genesis block', ()=>{
-       expect(blockchain.chain[0]).toEqual(Block.genesis()); 
+
+    it('the first block is genesis block', () => {
+        expect(blockchain.chain[0]).toEqual(Block.genesis());
     });
 
-    it('adds a new block', ()=>{
-        const data = 'arquivo.pdf';
+    it('adds a new block', () => {
+        let data = 'arquivo.pdf';
 
         blockchain.addBlock(data);
+
+        expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(data);
+    });
+
+    it('validates a valid chain - secondBlockchain', () => {
+        let data = 'R$ 500';
+
+        secondBlockchain.addBlock(data);
+
+        expect(blockchain.isValidChain(secondBlockchain.chain)).toBe(true);
+    });
+
+    it('invalidates a chain with a corrupt genesis block - secondBlockchain', () => {
+
+        secondBlockchain.chain[0].data = 'bug';
+
+        expect(blockchain.isValidChain(secondBlockchain.chain)).toBe(false);
+    });
+
+    it('invalidates a corrupt chain - secondBlockchain', () => {
+
+        let data = 'R$ 400';
+
+        secondBlockchain.addBlock(data);
+
+        secondBlockchain.chain[1].data = 'bug';
+
+        expect(blockchain.isValidChain(secondBlockchain.chain)).toBe(false);
+    });
+
+    it('replaces the chain with a valid chain', () => {
         
-        expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(data); 
-     });
+        let data = 'R$ 700';
+
+        secondBlockchain.addBlock(data);
+        blockchain.replaceChain(secondBlockchain.chain);
+
+        expect(blockchain.chain).toEqual(secondBlockchain.chain);
+
+    });
+
+    it('does not replace the chain with one of less or equal length', () => {
+        
+        let data = 'R$ 800';
+
+        blockchain.addBlock(data);
+        blockchain.replaceChain(secondBlockchain.chain);
+
+        expect(blockchain.chain).not.toEqual(secondBlockchain.chain);
+
+    });
+
+    it('does not replace the chain with invalid chain', () => {
+        
+        let data = 123;
+
+        secondBlockchain.addBlock(data);
+        blockchain.replaceChain(secondBlockchain.chain);
+
+        expect(blockchain.replaceChain).throws();
+
+    });
 })
