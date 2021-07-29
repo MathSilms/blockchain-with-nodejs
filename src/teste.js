19b6978d612c941e314153a6e9df2dc09b908641
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2021-07-29 16:09:47 -0300

    ajustes

--- src/teste.js
+++ src/teste.js
@@ -3,9 +3,9 @@ const Blockchain = require('./blockchain');
 
 
 
-let block1 = Block.mineBlock(Block.genesis(), '$500');
-let block2 = new Block('7657', '6854684asasdd', 'af5s4984dsa6s4589','100');
-let block3 = new Block('0000', 'sd6ad8wa4d6sa', 'ca4g5585bre9a1847','300');
+let block1 = Block.mineBlock(Block.genesis(), '500');
+let block2 = Block.mineBlock(block1, '100');
+let block3 = Block.mineBlock(block2, '2030');
 
 let blockchain = new Blockchain(block1);
 
@@ -30,5 +30,6 @@ let blockchainFork = new Blockchain(secondBlock1);
 // erro de corrente com tamanho Menor que a corrente atual
 blockchain.replaceChain(blockchainFork.chain);
 
+
 // erro de corrente invalida
-blockchain.replaceChain("corrente em formato invalido");
+blockchain.replaceChain("teste");
