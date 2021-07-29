50efc6d4204782a25eec90236684fd29e4f3351a
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2021-07-29 14:35:04 -0300

    criando arquivo para verificar toda a blockchain em funcionamento

--- src/teste.js
+++ src/teste.js
@@ -1,11 +1,34 @@
 const Block = require('./block');
+const Blockchain = require('./blockchain');
 
 
-const block = new Block('7657', '6854684asasdd', 'af5s4984dsa6s4589','100');
 
+let block1 = Block.mineBlock(Block.genesis(), '$500');
+let block2 = new Block('7657', '6854684asasdd', 'af5s4984dsa6s4589','100');
+let block3 = new Block('0000', 'sd6ad8wa4d6sa', 'ca4g5585bre9a1847','300');
 
-console.log(block.toString());
-console.log(Block.genesis().toString());
-const firstBlock = Block.mineBlock(Block.genesis(), '$500');
+let blockchain = new Blockchain(block1);
 
-console.log(firstBlock.toString());
+blockchain.addBlock(block2);
+blockchain.addBlock(block3);
+
+console.log("---------------------");
+console.log("bloco genesis :", Block.genesis());
+console.log("---------------------");
+console.log("primeiro bloco :",block1);
+console.log("---------------------");
+console.log("blocos:", [block1, block2, block3]);
+console.log("---------------------");
+console.log("blockchain :", blockchain.chain);
+console.log("---------------------");
+
+// // simulando um erro 
+
+let secondBlock1 = Block.mineBlock(Block.genesis(), '$900');
+let blockchainFork = new Blockchain(secondBlock1);
+
+// erro de corrente com tamanho Menor que a corrente atual
+blockchain.replaceChain(blockchainFork.chain);
+
+// erro de corrente invalida
+blockchain.replaceChain("corrente em formato invalido");
